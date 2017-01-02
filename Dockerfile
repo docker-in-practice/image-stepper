5aba9f1baf74acc1661d690f35a571c6ea55a898
Ian Miell <ian.miell@gmail.com>
2017-01-02 14:33:34 +0000

    install docker

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM ubuntu
+RUN apt-get update -y && apt-get install -y docker-client
 ADD image_stepper /usr/local/bin/image_stepper
 ENTRYPOINT ["/usr/local/bin/image_stepper"]
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Note that if you are using Mac/Windows then the volume mount of docker may not work.
-docker run -ti -v $(which docker):/usr/local/bin/docker dockerinpractice/image-stepper ubuntu latest
+docker run -ti -v /var/run/docker.sock:/var/run/docker.sock dockerinpractice/image-stepper ubuntu latest
