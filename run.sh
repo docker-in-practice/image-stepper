9428e5e812556a5688567b47c3cb1215d6fee762
Ian Miell <ian.miell@gmail.com>
2017-01-02 14:20:10 +0000

    args

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Note that if you are using Mac/Windows then the volume mount of docker may not work.
-docker run -ti -v $(which docker):/usr/local/bin/docker docker-in-practice/image_stepper
+docker run -ti -v $(which docker):/usr/local/bin/docker dockerinpractice/image-stepper ubuntu latest
